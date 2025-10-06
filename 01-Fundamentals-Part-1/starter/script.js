2235c52e8baeb5fc2c540b74af7e3aa107ddf01a
LiShaojun1126 <lshaojun1126@gmail.com>
2025-10-05 21:35:37 -0400

    p13 let const and var

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+console.log(40 + 8);
+
+let firstName = "shaojun";
+console.log(firstName)
+
+console.log(typeof firstName);
+
+let year;
+console.log(typeof year);
+
+console.log(typeof null); //this is a error, the type of null shoule be null
+
+const birthYear = 2002; //we must initialize a const variable when we declare it.
+
+//we should use const as many as possible, and never use var
+
