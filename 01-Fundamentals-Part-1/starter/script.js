906b05c9c04f14746dd661cba1a229c981ca738e
LiShaojun1126 <lshaojun1126@gmail.com>
2025-10-08 15:39:39 -0400

    code challenge 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,25 +1,25 @@
-let js = "amazing";
-console.log(40 + 8);
+// let js = "amazing";
+// console.log(40 + 8);
 
-let firstName = "shaojun";
-console.log(firstName);
+// let firstName = "shaojun";
+// console.log(firstName);
 
-console.log(typeof firstName);
+// console.log(typeof firstName);
 
-let year;
-console.log(typeof year);
+// let year;
+// console.log(typeof year);
 
-console.log(typeof null); //this is a error, the type of null shoule be null
+// console.log(typeof null); //this is a error, the type of null shoule be null
 
-const birthYear = 2002; //we must initialize a const variable when we declare it.
+// const birthYear = 2002; //we must initialize a const variable when we declare it.
 
 //we should use const as many as possible, and never use var
 
-const nowYear = 2025;
-const shaojunAge = nowYear - 2002;
-const yachenAge = nowYear - 2012;
+// const nowYear = 2025;
+// const shaojunAge = nowYear - 2002;
+// const yachenAge = nowYear - 2012;
 
-console.log(shaojunAge, yachenAge);
+// console.log(shaojunAge, yachenAge);
 
 // const markWeight = 78;
 // const markHeight = 1.69;
@@ -27,18 +27,52 @@ console.log(shaojunAge, yachenAge);
 // const johnWeight = 92;
 // const johnHeight = 1.95;
 
-const markWeight = 95;
-const markHeight = 1.88;
+// const markWeight = 95;
+// const markHeight = 1.88;
 
-const johnWeight = 85;
-const johnHeight = 1.76;
+// const johnWeight = 85;
+// const johnHeight = 1.76;
 
-const markBMI = markWeight / markHeight ** 2;
-const johnBMI = johnWeight / johnHeight ** 2;
+// const markBMI = markWeight / markHeight ** 2;
+// const johnBMI = johnWeight / johnHeight ** 2;
 
-const markHigherBMI = markBMI > johnBMI;
+// const markHigherBMI = markBMI > johnBMI;
 
-console.log("Mark's BMI is " + markBMI);
-console.log("John's BMI is " + johnBMI);
+// console.log("Mark's BMI is " + markBMI);
+// console.log("John's BMI is " + johnBMI);
 
-if (markHigherBMI) console.log("Mark has higher BMI");
+// if (markHigherBMI) console.log("Mark has higher BMI");
+
+//template literals
+// const job = "student";
+// const shao = `I'm ${firstName}, a ${birthYear} born ${job}`; //${} is called placeholder
+// console.log(shao);
+// console.log(`You can also use backticks in regular string.`);
+
+// console.log(`backticks
+// can also be used
+// in
+// multiple lines`);
+
+// const age = 22;
+
+// if (age >= 18) {
+//   console.log("I am able to drive! 🚗");
+// }
+
+// const birthYear = 2002;
+// let century;
+// if (birthYear >= 2000) {
+//   century = 21;
+// } else {
+//   century = 20;
+// }
+// console.log(century);
+
+// if (markBMI > johnBMI) {
+//   console.log("Mark's BMI is higher than John's.");
+//   console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+// } else {
+//   console.log("John's BMI is higher than Mark's.");
+//   console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+// }
