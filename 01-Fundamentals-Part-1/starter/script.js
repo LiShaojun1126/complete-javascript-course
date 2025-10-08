e006bfeab79c9fad86d735f3df4772b5d9cb31ab
LiShaojun1126 <lshaojun1126@gmail.com>
2025-10-07 23:01:56 -0400

    finished 015

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,8 @@
-let js = 'amazing';
+let js = "amazing";
 console.log(40 + 8);
 
 let firstName = "shaojun";
-console.log(firstName)
+console.log(firstName);
 
 console.log(typeof firstName);
 
@@ -15,3 +15,8 @@ const birthYear = 2002; //we must initialize a const variable when we declare it
 
 //we should use const as many as possible, and never use var
 
+const nowYear = 2025;
+const shaojunAge = nowYear - 2002;
+const yachenAge = nowYear - 2012;
+
+console.log(shaojunAge, yachenAge);
